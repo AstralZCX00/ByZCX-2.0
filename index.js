0ef60bf4048c722037c1848ea60edf53a4e21e5b
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-01 23:41:50 +0100

    Upload files

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
+
+client.login("OTQ4MjIyMzQ2ODUyOTU4MjI4.Yh4q4A.1Fr56bHI52M0VlbLKqxlJ6mJko4")
+
+client.on("ready", () => {
+console.log("bot online")
+})
+client.on("guildMemberAdd", (message) => {
+console.log ("nuovo utente")
+})
+
+client.on('guildMemberAdd', async (member) => {
+    const Embed1 = new Discord.MessageEmbed()
+       .setColor('#ff002b')
+       .setTitle('Benvenuto in ByZCX 2.0')
+       .setDescription('Benvenuto in ByZCX 2.0, per accedere ai canali sei pregato di cliccare sulla reazione in fondo al regolamento per confermare la presa visione')
+       .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg');
+    await member.send({ embeds: [Embed1] });
+
+})
