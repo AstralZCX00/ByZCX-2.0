d254b8389109ae8686104731730f3e64ecaeb737
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-02 20:51:37 +0100

    Update generale

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client(
-    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS"] }
 )
 
 client.login(process.env.token)
@@ -17,7 +17,18 @@ client.on('guildMemberAdd', async (member) => {
        .setColor('#ff002b')
        .setTitle('Benvenuto in ByZCX 2.0')
        .setDescription('Benvenuto in ByZCX 2.0, per accedere ai canali sei pregato di cliccare sulla reazione in fondo al regolamento per confermare la presa visione')
-       .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg');
+       .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg%27');
     await member.send({ embeds: [Embed1] });
 
+})
+
+client.on('guildMemberAdd', (member) => {
+    client.channels.cache.get('948660777177460817').setName(`All User: - ${member.guild.memberCount}`)
+    client.channels.cache.get('948660919175630898').setName(`Members: - ${member.guild.members.cache.filter(member => !member.user.bot).size}`)
+    client.channels.cache.get('948661031809450076').setName(`Bots: - ${member.guild.members.cache.filter(member => !member.user.bot).size}`)
+})
+client.on('guildMemberRemove', (member) => {
+    client.channels.cache.get('948660777177460817').setName(`All User: - ${member.guild.memberCount}`)
+    client.channels.cache.get('948660919175630898').setName(`Members: - ${member.guild.members.cache.filter(member => !member.user.bot).size}`)
+    client.channels.cache.get('948661031809450076').setName(`Bots: - ${member.guild.members.cache.filter(member => !member.user.bot).size}`)
 })
