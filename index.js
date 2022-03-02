09df270efe8cc2fb1d0444676e062a7adf1dff04
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-02 22:53:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ client.on('guildMemberAdd', async (member) => {
     await member.send({ embeds: [Embed1] });
 
 })
+client.on("messageCreate", (message) => {
+    
+    if (message.content == "!embed") {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("SERVER STATS OFF")
+            .setDescription("Le statistiche del server sono momentaneamente offline, tempo stimato risoluzione problema: 24h/72h")
+            .setThumbnail("https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg%27")
+
+        message.channel.send({ embeds: [embed] })
+    }
