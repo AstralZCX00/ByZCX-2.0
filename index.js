44dc264af9cde199d1fe653a1b6289c75cc4b48d
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-02 09:27:03 +0100

    update member count

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on('guildMemberAdd', async (member) => {
 
 })
 
-client.on('guildMemberAdd', member =>{
-    var canale = client.channels.cache.get('948493301328252979')
-    canale.setName('members: ' + member.guild.memberCount);
-})
+client.on("guildMemberAdd", member => {
+    var canale = client.channels.cache.get("948493301328252979")
+    canale.setName("members: " + member.guild.memberCount)
+});
