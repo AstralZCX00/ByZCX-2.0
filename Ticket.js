6511212f40ee14613ee89494ff766ae4134fd8d1
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-04 20:13:47 +0100

    Update Ticket.js

--- Ticket.js
+++ Ticket.js
@@ -16,6 +16,12 @@ client.on("messageCreate", message => {
 
         var row = new Discord.MessageActionRow()
             .addComponents(button1)
+        
+        var embedticket = new Discord.MessageEmbed()
+            .setColor('#ff002b')
+            .setTitle('APRI UN TICKET')
+            .setDescription('clicca sul bottone in basso per aprire un ticket e parlare direttamente con lo Staff')
+            .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg')
 
         message.channel.send({ embeds: [embedticket] , components: [row] })
     }
