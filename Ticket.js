de3da3ce3f4a293af4f5f1e17cf42f925e02d12c
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-04 17:11:34 +0100

    Update Ticket.js

--- Ticket.js
+++ Ticket.js
@@ -1,3 +1,49 @@
+//Prima di tutto mandare il messaggio del ticket
+client.on("messageCreate", message => {
+    if (message.content == "!comando") {
+        var button1 = new Discord.MessageButton()
+            .setLabel("Apri ticket")
+            .setCustomId("apriTicket")
+            .setStyle("PRIMARY")
+
+        var row = new Discord.MessageActionRow()
+            .addComponents(button1)
+
+        message.channel.send({ content: "Clicca sul bottone per aprire un ticket", components: [row] })
+    }
+})
+
+client.on("interactionCreate", interaction => {
+    if (interaction.customId == "apriTicket") {
+        interaction.deferUpdate()
+        if (interaction.guild.channels.cache.find(canale => canale.topic == `User ID: ${interaction.user.id}`)) {
+            interaction.user.send("Hai gia un ticket aperto").catch(() => { })
+            return
+        }
+        interaction.guild.channels.create(interaction.user.username, {
+            type: "text",
+            topic: `User ID: ${interaction.user.id}`,
+            parent: "idCategoria", //Settare la categoria,
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.id,
+                    deny: ["VIEW_CHANNEL"]
+                },
+                {
+                    id: interaction.user.id,
+                    allow: ["VIEW_CHANNEL"]
+                },
+                { //Aggiungere altri "blocchi" se si vogliono dare permessi anche a ruoli o utenti
+                    id: "idRuolo",
+                    allow: ["VIEW_CHANNEL"]
+                }
+            ]
+        }).then(canale => {
+            canale.send("Grazie per aver aperto un ticket")
+        })
+    }
+})
+
 client.on("messageCreate", message => {
     if (message.content == "!close") {
         var topic = message.channel.topic;
