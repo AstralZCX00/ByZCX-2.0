c1791ee5b102ee6a964f2bd140a7becbb34a9487
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-18 22:56:53 +0100

    Update tickets.js

--- tickets.js
+++ tickets.js
@@ -9,7 +9,7 @@ client.on("messageCreate", message => {
         var row = new Discord.MessageActionRow()
             .addComponents(button1)
 
-        message.channel.send({ content: "Clicca sul bottone per aprire un ticket", components: [row] })
+        message.channel.send({ embeds: [embedticket], components: [row] })
     }
 })
 
@@ -123,4 +123,13 @@ client.on("messageCreate", message => {
             message.channel.send("Non puoi utilizzare questo comando qui")
         }
     }
+})
+
+client.on("messageCreate", (message) => {
+        var embedticket = new Discord.MessageEmbed()
+        .setColor('#ff002b')
+        .setTitle('TICKET')
+        .setDescription('Clicca sul bottone per aprire un ticket')
+        .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg')
+        message.channel.send({ embeds: [embedticket] })
 })
