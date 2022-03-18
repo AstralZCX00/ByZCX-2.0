6e5e5d707ecf6cd4400141fe2bf3e38266da1684
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-18 23:04:22 +0100

    Update tickets.js

--- tickets.js
+++ tickets.js
@@ -125,11 +125,3 @@ client.on("messageCreate", message => {
     }
 })
 
-client.on("messageCreate", (message) => {
-        var embedticket = new Discord.MessageEmbed()
-        .setColor('#ff002b')
-        .setTitle('TICKET')
-        .setDescription('Clicca sul bottone per aprire un ticket')
-        .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg')
-        message.channel.send({ embeds: [embedticket] })
-})
