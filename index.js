37f448329cf48849dca2861e12d20e1475bcdeac
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-18 22:03:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,16 +34,6 @@ client.on("messageCreate", (message) => {
 })
 
 client.on("messageCreate", (message) => {
-        var embedticket = new Discord.MessageEmbed()
-        .setColor('#ff002b')
-        .setTitle('APRI UN TICKET')
-        .setDescription('clicca sul bottone in basso per aprire un ticket e parlare direttamente con lo Staff')
-        .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg')
-        message.channel.send({ embeds: [embed2] })
-    }
-)
-
-client.on("messageCreate", (message) => {
     if (message.content == "!/prezzi") {
         var embed3 = new Discord.MessageEmbed()
         .setColor('#ff002b')
