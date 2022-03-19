08c242ec7cfd14e35d8a9e76185c36a3821e4098
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-03-19 08:42:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -178,143 +178,19 @@ client.on("messageCreate", message => {
         .setThumbnail('https://cdn.discordapp.com/attachments/947563942866198549/948322773246279730/ByZCX.jpg')
         
         var button1 = new Discord.MessageButton()
-            .setLabel("Assistenza")
-            .setCustomId("apriTicket1")
+            .setLabel("Apri ticket")
+            .setCustomId("apriTicket")
             .setStyle("PRIMARY")
 
-        var row1 = new Discord.MessageActionRow()
+        var row = new Discord.MessageActionRow()
             .addComponents(button1)
 
-        var button2 = new Discord.MessageButton()
-            .setLabel("Grafica")
-            .setCustomId("apriTicket2")
-            .setStyle("PRIMARY")
-
-        var row2 = new Discord.MessageBotton()
-            .addComponents(button2)
-
-        message.channel.send({ embeds: [embedticket], components: [row1] [row2] })
+        message.channel.send({ embeds: [embedticket], components: [row] })
     }
 })
 
 client.on("interactionCreate", interaction => {
-    if (interaction.customId == "apriTicket1") {
-        interaction.deferUpdate()
-        if (interaction.guild.channels.cache.find(canale => canale.topic == `User ID: ${interaction.user.id}`)) {
-            interaction.user.send("Hai gia un ticket aperto").catch(() => { })
-            return
-        }
-        interaction.guild.channels.create(interaction.user.username, {
-            type: "text",
-            topic: `User ID: ${interaction.user.id}`,
-            parent: "949406150011531335", //Settare la categoria,
-            permissionOverwrites: [
-                {
-                    id: interaction.guild.id,
-                    deny: ["VIEW_CHANNEL"]
-                },
-                {
-                    id: interaction.user.id,
-                    allow: ["VIEW_CHANNEL"]
-                },
-                {
-                    id: "948171930475851856",
-                    allow: ["VIEW_CHANNEL"]
-                },
-                {
-                    id: "954391756688986143",
-                    allow: ["VIEW_CHANNEL"]
-                }
-            ]
-        }).then(canale => {
-            canale.send("Grazie per aver aperto un ticket")
-        })
-    }
-})
-
-client.on("messageCreate", message => {
-    if (message.content == "!close") {
-        var topic = message.channel.topic;
-        if (!topic) {
-            message.channel.send("Non puoi utilizzare questo comando qui");
-            return
-        }
-        if (topic.startsWith("User ID:")) {
-            var idUtente = topic.slice(9);
-            if (message.author.id == idUtente || message.member.permissions.has("MANAGE_CHANNELS")) {
-                message.channel.delete();
-            }
-        }
-        else {
-            message.channel.send("Non puoi utilizzare questo comando qui")
-        }
-    }
-    if (message.content.startsWith("!add")) {
-        var topic = message.channel.topic;
-        if (!topic) {
-            message.channel.send("Non puoi utilizzare questo comando qui");
-            return
-        }
-        if (topic.startsWith("User ID:")) {
-            var idUtente = topic.slice(9);
-            if (message.author.id == idUtente || message.member.permissions.has("MANAGE_CHANNELS")) {
-                var utente = message.mentions.members.first();
-                if (!utente) {
-                    message.channel.send("Inserire un utente valido");
-                    return
-                }
-                var haIlPermesso = message.channel.permissionsFor(utente).has("VIEW_CHANNEL", true)
-                if (haIlPermesso) {
-                    message.channel.send("Questo utente ha gia accesso al ticket")
-                    return
-                }
-                message.channel.permissionOverwrites.edit(utente, {
-                    VIEW_CHANNEL: true
-                })
-                message.channel.send(`${utente.toString()} è stato aggiunto al ticket`)
-            }
-        }
-        else {
-            message.channel.send("Non puoi utilizzare questo comando qui")
-        }
-    }
-    if (message.content.startsWith("!remove")) {
-        var topic = message.channel.topic;
-        if (!topic) {
-            message.channel.send("Non puoi utilizzare questo comando qui");
-            return
-        }
-        if (topic.startsWith("User ID:")) {
-            var idUtente = topic.slice(9);
-            if (message.author.id == idUtente || message.member.permissions.has("MANAGE_CHANNELS")) {
-                var utente = message.mentions.members.first();
-                if (!utente) {
-                    message.channel.send("Inserire un utente valido");
-                    return
-                }
-                var haIlPermesso = message.channel.permissionsFor(utente).has("VIEW_CHANNEL", true)
-                if (!haIlPermesso) {
-                    message.channel.send("Questo utente non ha già accesso al ticket")
-                    return
-                }
-                if (utente.permissions.has("MANAGE_CHANNELS")) {
-                    message.channel.send("Non puoi rimuovere questo utente")
-                    return
-                }
-                message.channel.permissionOverwrites.edit(utente, {
-                    VIEW_CHANNEL: false
-                })
-                message.channel.send(`${utente.toString()} è stato rimosso al ticket`)
-            }
-        }
-        else {
-            message.channel.send("Non puoi utilizzare questo comando qui")
-        }
-    }
-})
-
-lient.on("interactionCreate", interaction => {
-    if (interaction.customId == "apriTicket2") {
+    if (interaction.customId == "apriTicket") {
         interaction.deferUpdate()
         if (interaction.guild.channels.cache.find(canale => canale.topic == `User ID: ${interaction.user.id}`)) {
             interaction.user.send("Hai gia un ticket aperto").catch(() => { })
