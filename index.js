dcd6792e02bcc8f4870113fb51ff6226e1ee80f6
AstralZCX00 <100731953+AstralZCX00@users.noreply.github.com>
2022-05-21 21:22:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS"] }
 );
 
-client.login(process.env.token)
+client.login("OTQ4MjIyMzQ2ODUyOTU4MjI4.GQtad4.EPdbyDXkHmqUWdqfBlBp6-AD-4JSGIOytFnLhk")
 
 client.on("ready", () => {
 console.log("bot online")
